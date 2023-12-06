bb7eb0d42530f059223ea29cb5e377c3c524ac3c
nimbul <nimbuldev@gmail.com>
2023-12-06 15:55:53 -0600

    Add Inventory Organizing Features

---
+++ SPT-Inventory-Organizing-features-SIT/reflections/Extensions/LootItemClassReflector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryOrganizingFeatures.Reflections.Extensions
+{
+    internal static class LootItemClassReflector
+    {
+        public static Type ReflectedType = typeof(CompoundItem);
+
+        public static Grid[] RGrids(this CompoundItem item)
+        {
+            var grids = item.GetFieldValue<object[]>("Grids");
+            var reflectedGrids = new Grid[grids.Length];
+            for(int i = 0; i < grids.Length; i++) {
+                reflectedGrids[i] = new Grid(grids[i]);
+            }
+            return reflectedGrids;
+        }
+    }
+}
