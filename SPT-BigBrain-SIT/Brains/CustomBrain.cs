72ae6c10958295418114989bdba0ac43067a53b2
Ryan <void.big@gmail.com>
2024-02-29 23:44:30 -0800

    BigBrain

---
+++ SPT-BigBrain-SIT/Brains/CustomBrain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// TODO: Implement full custom brains
+namespace DrakiaXYZ.BigBrain.Brains
+{
+    public abstract class CustomBrain
+    {
+    }
+}
