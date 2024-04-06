d0dd7f4efe39655d9da92b336ec132608812db1f
umbraprior <110497713+umbraprior@users.noreply.github.com>
2024-04-06 17:37:26 -0400

    Ported Prop's In-Raid Modding
    Created for EFT 0.14.1.3.29351 & SIT 1.10.8854.18454

---
+++ PIRM-SIT/PIRMPlugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using BepInEx;
+using VersionChecker;
+
+namespace PIRM
+{
+
+    [BepInPlugin("com.dvize.PIRM", "dvize.PIRM", "1.8.0")]
+    //[BepInDependency("com.spt-aki.core", "3.7.4")]
+    class PIRMPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+        }
+        private void Start()
+        {
+            new PIRMMethod17Patch().Enable();
+            new InteractionsHandlerPatch().Enable();
+            new ItemCheckAction().Enable();
+            new EFTInventoryLogicModPatch().Enable();
+            new LootItemApplyPatch().Enable();
+            //new SlotMethod_2Patch().Enable();  - seems to enable all equipment slots to take any item instead of the weapon slot.  error coming from weapon check itself somewhere.  says multitool required
+        }
+
+        
+    }
+}
