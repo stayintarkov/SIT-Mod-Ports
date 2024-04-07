b1d25fea7951226bc9d19fe077a9892d1c985bcd
umbraprior <110497713+umbraprior@users.noreply.github.com>
2024-04-06 23:54:33 -0400

    Ported USE ANY ITEMS ANYWHERE
    Created for EFT 0.14.1.3.29351 & SIT 1.10.8854.18454

---
+++ UseItemsAnywhere-SIT/UseItemsFromAnywhere/Patches/Patches.cs
@@ -0,0 +1,117 @@
+﻿using StayInTarkov;
+using Comfort.Common;
+using EFT.InventoryLogic;
+using EFT.UI;
+using HarmonyLib;
+using System.Linq;
+using System.Reflection;
+
+namespace UseItemsFromAnywhere
+{
+    public class UseItemsFromAnywhere
+    {
+        private static string[] items =
+        {
+            "62178c4d4ecf221597654e3d", // Red Flare
+            "624c0b3340357b5f566e8766", // Yellow Flare
+            "6217726288ed9f0845317459", // green Flare
+            "62178be9d0050232da3485d9"  // white Flare
+        };
+
+        public class IsAtBindablePlace : ModulePatch
+        {
+            protected override MethodBase GetTargetMethod() =>
+                typeof(InventoryControllerClass).GetMethod("IsAtBindablePlace", BindingFlags.Public | BindingFlags.Instance);
+
+            [PatchPostfix]
+            private static void Postfix(InventoryControllerClass __instance, ref bool __result, ref Item item)
+            {
+                Inventory inventory =
+                    (Inventory)AccessTools.Property(typeof(InventoryControllerClass), "Inventory").GetValue(__instance);
+
+                if (item is Weapon || item is GrenadeClass || item is MedsClass
+                    || item is FoodClass || item is GItem9 || item is GGItem15
+                    || item is RecodableItemClass || item.GetItemComponent<KnifeComponent>() != null
+                    || items.Contains(item.TemplateId))
+                {
+                    if (inventory.Equipment.Contains(item))
+                    {
+                        __result = true;
+                        return;
+                    }
+                    else
+                    {
+#if DEBUG
+                        ConsoleScreen.Log($"{item.TemplateId} is not in the players inventory");
+#endif
+                        __result = false;
+                        return;
+                    }
+                }
+                else
+                {
+#if DEBUG
+                    ConsoleScreen.Log($"Item {item.TemplateId} not of bindable type");
+#endif
+                    return;
+                }
+            }
+        }
+
+        public class IsAtReachablePlace : ModulePatch
+        {
+            protected override MethodBase GetTargetMethod() =>
+                typeof(InventoryControllerClass).GetMethod("IsAtReachablePlace", BindingFlags.Public | BindingFlags.Instance);
+
+
+            [PatchPostfix]
+            private static void Postfix(InventoryControllerClass __instance, ref bool __result, ref Item item)
+            {
+                Inventory inventory =
+                    (Inventory)AccessTools.Property(typeof(InventoryControllerClass), "Inventory").GetValue(__instance); 
+
+                if (inventory.Equipment.Contains(item))
+                {
+                    __result = true;
+                    return;
+                }
+                else
+                {
+#if DEBUG
+                        ConsoleScreen.Log($"{item.TemplateId} is not in the players inventory");
+#endif
+                    __result = false;
+                    return;
+                }
+            }
+        }
+
+        public class UnbindItemPatch : ModulePatch
+        {
+            protected override MethodBase GetTargetMethod() =>
+                typeof(InventoryControllerClass).GetMethod("UnbindItem", BindingFlags.Public | BindingFlags.Instance);
+
+
+            [PatchPostfix]
+            private static void Postfix(InventoryControllerClass __instance, ref bool __result, ref Item item)
+            {
+                Inventory inventory =
+                    (Inventory)AccessTools.Property(typeof(InventoryControllerClass), "Inventory").GetValue(__instance);
+
+                if (inventory.Equipment.Contains(item))
+                {
+                    __result = true;
+                    return;
+                }
+                else
+                {
+#if DEBUG
+                    ConsoleScreen.Log($"{item.TemplateId} is not in the players inventory");
+#endif
+                    __result = false;
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ UseItemsAnywhere-SIT/UseItemsFromAnywhere/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using DrakiaXYZ.VersionChecker;
+using EFT.InventoryLogic;
+using HarmonyLib;
+using System;
+using static UseItemsFromAnywhere.UseItemsFromAnywhere;
+
+namespace UseItemsFromAnywhere
+{
+    [BepInPlugin("com.dirtbikercj.useFromAnywhere", "Use items anywhere", "1.1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const int TarkovVersion = 29351;
+
+        public static Plugin instance;
+
+        public static EquipmentSlot[] extendedFastAccessSlots =
+        {
+            EquipmentSlot.Pockets,
+            EquipmentSlot.TacticalVest,
+            EquipmentSlot.Backpack,
+            EquipmentSlot.SecuredContainer,
+            EquipmentSlot.ArmBand
+        };
+
+        internal void Awake()
+        {
+            if (!VersionChecker.CheckEftVersion(Logger, Info, Config))
+            {
+                throw new Exception("Invalid EFT Version");
+            }
+
+            instance = this;
+            DontDestroyOnLoad(this);
+
+            var fastAccessSlots = AccessTools.Field(typeof(Inventory), "FastAccessSlots");
+            fastAccessSlots.SetValue(fastAccessSlots, extendedFastAccessSlots);
+
+            new IsAtBindablePlace().Enable();
+            new IsAtReachablePlace().Enable();
+        }
+    }
+}
