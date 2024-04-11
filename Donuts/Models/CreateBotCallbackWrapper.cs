a02a29cf8fa2b7620bf5e9d0a859c650336e83dc
legendnation <legendnation3@gmail.com>
2024-04-11 15:57:22 +0300

    Swag + Donuts 3.3.3
    Ported swag + Donuts 3.3.3

---
+++ Donuts/Models/CreateBotCallbackWrapper.cs
@@ -0,0 +1,24 @@
+﻿using EFT;
+using System.Diagnostics;
+using static Donuts.DonutComponent;
+using BotCacheClass = Data1;
+
+namespace Donuts
+{
+    // Wrapper around method_10 called after bot creation, so we can pass it the BotCacheClass data
+
+    internal class CreateBotCallbackWrapper
+    {
+        public BotCacheClass botData;
+        public Stopwatch stopWatch = new Stopwatch();
+
+        public void CreateBotCallback(BotOwner bot)
+        {
+            bool shallBeGroup = botData.SpawnParams?.ShallBeGroup != null;
+
+            // I have no idea why BSG passes a stopwatch into this call...
+            stopWatch.Start();
+            methodCache["method_10"].Invoke(botSpawnerClass, new object[] { bot, botData, null, shallBeGroup, stopWatch });
+        }
+    }
+}
