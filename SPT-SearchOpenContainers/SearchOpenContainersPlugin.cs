37331a1dc8f660eaaeb6e0645099da525cf579c1
Samuel Schlesinger <4713229+SamuelMS@users.noreply.github.com>
2024-05-09 23:35:05 -0700

    port SearchOpenContainers

---
+++ SPT-SearchOpenContainers/SearchOpenContainersPlugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using DrakiaXYZ.SearchOpenContainers.Patches;
+
+namespace DrakiaXYZ.SearchOpenContainers
+{
+    [BepInPlugin("xyz.drakia.searchopencontainers", "DrakiaXYZ-SearchOpenContainers", "1.0.2")]
+    public class SearchOpenContainersPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo("[SearchOpenContainers] Loading...");
+
+            new ContainerMenuPatch().Enable();
+
+            Logger.LogInfo("[SearchOpenContainers] Loaded!");
+        }
+    }
+}
